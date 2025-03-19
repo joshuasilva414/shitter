94ce36fcb9cc525b048005f40ffcd69abf6df89e
joshuasilva414 <joshuasilva414@gmail.com>
2025-03-19 10:21:13 -0500

    update tsconfig

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { seed } from "drizzle-seed";
-import { users, posts } from "./schema.js";
+import { users, posts } from "./schema";
 import { drizzle } from "drizzle-orm/libsql";
 
 const db = drizzle("file:./db.sqlite");
